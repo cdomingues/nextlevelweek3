60b81daf26ed3a7027d246348d59bf137e17ff7b
Carlos Domingues <domingues_santos@hotmail.com>
2020-10-15 16:07:11 -0300

    criando rotas

---
+++ src/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import OrphanagesMap from './pages/OrphanagesMap';
+
+function Routes(){
+    return(
+        <BrowserRouter>
+        <Route path="/" exact component={Landing} ></Route>
+        <Route path="/app" component={OrphanagesMap}></Route>
+
+
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
