892b97aa26143e212916b465da6a0dc5f5d5f28a
Carlos Domingues <domingues_santos@hotmail.com>
2020-10-16 11:54:04 -0300

    add pagina com mapa e finalizando dia 1 nlw

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import mapMarkerImg from '../images/mapmark.svg'
+import {Link} from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi';
+import '../styles/pages/orphanages-map.css';
+import {Map, TileLayer} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
 
 function OrphanagesMap(){
 
 return(
-    <h1>Hello world</h1>
+    <div id="page-map">
+
+    <aside>
+        <header>
+            <img src={mapMarkerImg} alt="Happy" />
+            <h2>Escolha um orfanato no mapa</h2>
+            <p>Muitas crianças estão esperando a sua visita :) </p>
+        </header>
+        <footer>
+            <strong>Mogi das Cruzes</strong>
+            <span>São Paulo</span>
+        </footer>
+    </aside>
+    <Map 
+    center={[-23.5394671,-46.2674354]} 
+    zoom={15}
+    style={{width:'100%', height:'100%'}}
+    >
+    <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    </Map>
+    <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF"/>
+    </Link>
+
+
+    </div>
 );
 }
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -6,12 +6,12 @@ import OrphanagesMap from './pages/OrphanagesMap';
 function Routes(){
     return(
         <BrowserRouter>
+        <Switch>
         <Route path="/" exact component={Landing} ></Route>
         <Route path="/app" component={OrphanagesMap}></Route>
-
+        </Switch>
 
         </BrowserRouter>
     );
 }
-
 export default Routes;
